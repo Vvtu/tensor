2ce13b7ad31acf48144f376cbc0d0f6fd0f75d9c
Viacheslav <v.tuev@anoitr.ru>
2020-12-09 19:23:17 +0300

    init

---
+++ tensor-back/server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+
+// константы
+const port = 8080;
+const host = '0.0.0.0';
+
+// приложение
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World 222');
+});
+
+app.listen(port, host);
+console.log(`running on http://${host}:${port}`);
+console.log(`process.version = ${process.version}`);
---
+++ tensor-front/src/App.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+//@ts-ignore
+const tf = window.tf;
+
+// Define a model for linear regression.
+const model = tf.sequential();
+model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+
+model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
+
+// Generate some synthetic data for training.
+const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
+const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+
+// Train the model using the data.
+model.fit(xs, ys, { epochs: 10 }).then(() => {
+  // Use the model to do inference on a data point the model hasn't seen before:
+  model.predict(tf.tensor2d([5], [1, 1])).print();
+  // Open the browser devtools to see the output
+});
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <div
+          className="App-link"
+          onClick={() => {
+            console.log('shareRet prep = ');
+            const data = {
+              title: 'web.dev',
+              text: 'Check out web.dev.',
+              url: 'https://web.dev/',
+            };
+
+            if (navigator.share) {
+              navigator
+                .share(data)
+                .then(() => console.log('Successful share'))
+                .catch((error) => console.log('Error sharing', error));
+            } else {
+              console.log('navigator.share not found');
+            }
+          }}
+        >
+          Share link 222
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
