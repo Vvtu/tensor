986898fa33e315c7d761471134be943ac393b714
Viacheslav <v.tuev@anoitr.ru>
2020-12-28 19:53:06 +0300

    fix webcam

--- tensor-front/src/TeachableMachine/index.tsx
+++ tensor-front/src/TeachableMachine/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Webcam from 'react-webcam';
+import ReactWebcam from 'react-webcam';
 
 import ErrorBoundaries from '../ErrorBoundaries';
 
@@ -12,49 +12,39 @@ console.log('tf = ', tf);
 console.log('tfvis = ', tfvis);
 
 const videoConstraints = {
-  width: 800,
-  height: 720,
+  width: 300,
+  height: 300,
   facingMode: 'environment',
 };
 
 function App() {
   const [data, setData] = React.useState<any>();
 
-  React.useEffect(() => {
-    // getData().then((cleaned) => setData(cleaned));
-  }, []);
+  React.useEffect(() => {}, []);
 
   React.useEffect(() => {
-    const fn = async () => {
-      // if (data.length !== 0) {
-      //   const model = createModel();
-      //   ploteData(data, model);
-      //   const tensorData = convertToTensor(data);
-      //   console.log('tensor = ', tensorData);
-      //   const { inputs, labels } = tensorData;
-      //   await trainModel(model, inputs, labels);
-      //   testModel(model, data, tensorData);
-      // }
-    };
+    const fn = async () => {};
     fn();
   }, [data]);
 
   const webcamRef = React.useRef(null);
 
   const capture = React.useCallback(() => {
-    //@ts-ignore
-    const imageSrc = webcamRef.current && webcamRef.current.getScreenshot();
-    console.log('imageSrc = ', imageSrc);
-    //@ts-ignore
-    setData(imageSrc && imageSrc.length);
-  }, [webcamRef]);
+    if (webcamRef.current) {
+      //@ts-ignore
+      const imageSrc = webcamRef.current.getScreenshot();
+      console.log('imageSrc = ', imageSrc);
+      setData(imageSrc && imageSrc.length);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [webcamRef?.current]);
 
   return (
     <div className="App">
       <ErrorBoundaries>
         <header className="App-header">
           {`data count = ${data}`}
-          <Webcam
+          <ReactWebcam
             audio={false}
             mirrored={true}
             height={videoConstraints.height}
