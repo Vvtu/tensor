8ccd722626e4e3353958e880aec859fa9b847bc8
Viacheslav <v.tuev@anoitr.ru>
2021-01-01 14:09:20 +0300

    fix webcam working

--- tensor-front/src/TeachableMachine/index.tsx
+++ tensor-front/src/TeachableMachine/index.tsx
@@ -19,6 +19,13 @@ const videoConstraints = {
 
 let count = 0;
 
+const URL = 'https://teachablemachine.withgoogle.com/models/G9cPc7f35/';
+const modelURL = URL + 'model.json';
+const metadataURL = URL + 'metadata.json';
+let model: any;
+let webcam: any;
+let maxPredictions: number;
+
 function App() {
   const captureTimeoutId = React.useRef<NodeJS.Timeout>();
   const [capture, setCapture] = React.useState<boolean>(false);
@@ -36,18 +43,69 @@ function App() {
   }, [captureTimeoutId.current]);
 
   React.useEffect(() => {
+    console.log('capture = ', capture);
     if (capture) {
-      delay(100).then(() => {
-        const newResult = [...result, count++];
-        if (newResult.length > 5) {
-          newResult.shift();
-        }
-        setResult(newResult);
-      });
+      // More API functions here:
+      // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
+
+      // the link to your model provided by Teachable Machine export panel
+
+      // Load the image model and setup the webcam
+      async function init() {
+        // load the model and metadata
+        // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+        // or files from your local hard drive
+        // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+        //@ts-ignore
+        const { tmImage } = window;
+        model = await tmImage.load(modelURL, metadataURL);
+        maxPredictions = model.getTotalClasses();
+        console.log('maxPredictions = ', maxPredictions);
+
+        // Convenience function to setup a webcam
+        const flip = false; // whether to flip the webcam
+        webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
+        await webcam.setup(); // request access to the webcam
+        await webcam.play();
+        window.requestAnimationFrame(loop);
+
+        // append elements to the DOM
+        //@ts-ignore
+        document.getElementById('webcam-container').appendChild(webcam.canvas);
+        // labelContainer = document.getElementById('label-container');
+        // for (let i = 0; i < maxPredictions; i++) {
+        //   // and class labels
+        //   labelContainer.appendChild(document.createElement('div'));
+        // }
+      }
+
+      async function loop() {
+        webcam.update(); // update the webcam frame
+        await predict();
+        await delay(100);
+        window.requestAnimationFrame(loop);
+      }
+
+      // run the webcam image through the image model
+      async function predict() {
+        // predict can take in an image, video or canvas html element
+        //@ts-ignore
+        const prediction = await model.predict(webcam.canvas);
+        console.log('prediction = ', prediction);
+        // setResult(prediction);
+      }
+      init();
+      // delay(100).then(() => {
+      //   const newResult = [...result, count++];
+      //   if (newResult.length > 5) {
+      //     newResult.shift();
+      //   }
+      //   setResult(newResult);
+      // });
     }
   }, [capture, result]);
 
-  const webcamRef = React.useRef(null);
+  // const webcamRef = React.useRef(null);
 
   // const capture = React.useCallback(() => {
   //   if (webcamRef.current) {
@@ -64,7 +122,8 @@ function App() {
       <ErrorBoundaries>
         <header className="App-header">
           <div>{`result = ${JSON.stringify(result)}`}</div>
-          <ReactWebcam
+          <div id="webcam-container">webcam-container</div>
+          {/* <ReactWebcam
             audio={false}
             mirrored={true}
             height={videoConstraints.height}
@@ -73,7 +132,7 @@ function App() {
             screenshotQuality={0.92}
             width={videoConstraints.width}
             videoConstraints={videoConstraints}
-          />
+          /> */}
           <button
             onClick={() => {
               setCapture(!capture);
